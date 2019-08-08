c6c7e82137288dd062c4611ca26c11db89096eb7
Dionisiu Brovka <d.brovka2013@yandex.ru>
2019-08-08 13:25:55 +0300

    Initial

---
+++ homework1/src/main/java/com/app/epamLearn/App.java
@@ -0,0 +1,13 @@
+package com.app.epamLearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "----------- Optional Task 1 : " );
+    }
+}
